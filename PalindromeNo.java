050ae5b17802a68d89b1476d054e4d7865844c77
Priyanka-saw <priyankasaw51@gmail.com>
2025-08-31 00:27:09 +0530

    day1

---
+++ PalindromeNo.java
@@ -0,0 +1,18 @@
+
+public class PalindromeNo {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(212));
+    }
+     static boolean isPalindrome(int x) {
+        int xO = x, xN = 0;
+            while(x>0){
+                int R = x%10;
+                xN = xN*10 + R;
+                x = x/10;
+            }
+
+        if (xO == xN) return true;
+        return false; 
+    }
+}
+
