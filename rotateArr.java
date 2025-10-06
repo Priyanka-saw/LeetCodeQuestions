a06be483bd53e9238cb3c885e60dbea0cf26e423
Priyanka-saw <priyankasaw51@gmail.com>
2025-10-06 23:52:09 +0530

    day_2

---
+++ rotateArr.java
@@ -0,0 +1,36 @@
+public class rotateArr {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int k = 3;
+        rotate(arr, k);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+
+        k = k % n; // In case k is greater than the length of the array
+
+        // reverse the first k elements
+        reverse(nums, 0, n - 1);
+
+        // reverse the remaining n-k elements
+        reverse(nums, 0, k - 1);
+
+        // reverse the entire array
+        reverse(nums, k, n - 1);
+    }
+
+     static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}   
