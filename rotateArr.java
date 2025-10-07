7e86c56f3a84eff729099f3915dcc3e030e5fe7b
Priyanka-saw <priyankasaw51@gmail.com>
2025-10-07 15:20:52 +0530

    day_2_minor changes

--- rotateArr.java
+++ rotateArr.java
@@ -1,11 +1,10 @@
+import java.util.Arrays;
 public class rotateArr {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7};
         int k = 3;
         rotate(arr, k);
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
+       System.out.println(Arrays.toString(arr));
     }
 
     public static void rotate(int[] nums, int k) {
