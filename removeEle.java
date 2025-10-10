c194607f5efc5eff293223d8733e7a20fc2836e5
Priyanka-saw <priyankasaw51@gmail.com>
2025-10-10 13:45:32 +0530

    day_6

--- removeEle.java
+++ removeEle.java
@@ -1,10 +1,10 @@
 public class removeEle {
     public static void main(String[] args){
         int[] arr = {3, 3, 4, 4, 6, 4};
-        System.out.println(RemoveEle(arr, 4));
+        System.out.println(RemoveElement(arr, 4));
     }
     
-    static int RemoveEle(int[] nums, int val){
+    static int RemoveElement(int[] nums, int val){
         int i = 0;
         for(int j = 0; j < nums.length; j++){
             if(nums[j] != val){
