d2c760c482b7ebec73b7007b84d6e32963014c05
Priyanka-saw <priyankasaw51@gmail.com>
2025-10-12 18:31:04 +0530

    count prime

---
+++ countPrime.java
@@ -0,0 +1,24 @@
+public class countPrime {
+    public static void main(String[] args){
+        int n = 10;
+        System.out.println(countPrimes(n));
+    }
+    static int countPrimes(int n){
+        if(n < 3) return 0;
+        boolean[] isPrime = new boolean[n];
+        for(int i = 2; i < n; i++){
+            isPrime[i] = true;
+        }
+        for(int i = 2; i * i < n; i++){
+            if(!isPrime[i]) continue;
+            for(int j = i * i; j < n; j += i){
+                isPrime[j] = false;
+            }
+        }
+        int count = 0;
+        for(int i = 2; i < n; i++){
+            if(isPrime[i]) count++;
+        }
+        return count;
+    }
+}
