777b2f3df766e9315937ee829411acd9862c07a0
Priyanka-saw <priyankasaw51@gmail.com>
2025-10-14 21:48:08 +0530

    Roman to integer

---
+++ romanToInteger.java
@@ -0,0 +1,34 @@
+public class romanToInteger {
+    public static void main(String[] args){
+        String s = "L";
+        int result = romanToInt(s);
+        System.out.println(result);
+    }
+    static int romanToInt(String s) {
+        int total = 0;
+        int prevValue = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            int currentValue = getValue(s.charAt(i));
+            if(currentValue < prevValue){
+                total -= currentValue;
+            } else {
+                total += currentValue;
+            }
+            prevValue = currentValue;
+        }
+        return total;
+    }
+    static int getValue(char c){
+        switch(c){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+}
