8e4a70d5178b520f5b79658485925deb4f1c75a7
Priyanka-saw <priyankasaw51@gmail.com>
2025-10-17 23:16:41 +0530

    length_of_the last_digit

---
+++ lengthOfLastDigit.java
@@ -0,0 +1,24 @@
+public class lengthOfLastDigit {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println(lengthOfLastDigit(str));
+    }
+    static int lengthOfLastDigit(String s) {
+        int length = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Count the length of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
+
