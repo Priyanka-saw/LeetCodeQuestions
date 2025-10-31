cf7515b40a82174f9742b2039c5c8b777763efc7
Priyanka-saw <priyankasaw51@gmail.com>
2025-10-31 22:41:31 +0530

    remove duplicates from sorted array 2

---
+++ removeDupSort.java
@@ -0,0 +1,22 @@
+public class removeDupSort {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 2, 3, 4, 4, 5 };
+        int newLength = removeDuplicates(arr);
+        System.out.println("New length: " + newLength);
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
